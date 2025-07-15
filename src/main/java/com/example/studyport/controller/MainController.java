497b482685c4bb4084b80013513601a234a44da8
harry <lhcplay1@gmail.com>
2025-07-16 07:06:02 +0900

    Revert "MainPage Login ID insertion"
    This reverts commit fa9c14ae77c155901fbae0709485cb49bf7ed682.

--- src/main/java/com/example/studyport/controller/MainController.java
+++ src/main/java/com/example/studyport/controller/MainController.java
@@ -3,11 +3,8 @@ package com.example.studyport.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 @Controller
 @Log4j2
@@ -15,12 +12,13 @@ import java.security.Principal;
 public class MainController {
 
     @GetMapping("/")
-    public String main(Principal principal, Model model) {
-        String email = principal.getName();
-        model.addAttribute("email", email);
+    public String main() {
 
         return "main";
     }
 
-
+    @GetMapping("/main")
+    public String mainTest(){
+        return "main2";
+    }
 }
