1c7c43c70426afc5b2e104de7d63d9f06b8043a6
hyochan <hyochan.lee91@gmail.com>
2025-08-07 15:04:48 +0900

    create 지역, 비공개(패스워드), topic, member_id(leader) 추가

--- src/main/java/com/example/studyport/controller/StudyController.java
+++ src/main/java/com/example/studyport/controller/StudyController.java
@@ -1,13 +1,16 @@
 package com.example.studyport.controller;
 
 import com.example.studyport.dto.CategoryDTO;
+import com.example.studyport.dto.MembersDTO;
 import com.example.studyport.dto.StudyDTO;
 import com.example.studyport.entity.Category;
+import com.example.studyport.entity.Members;
 import com.example.studyport.repository.CategoryRepository;
 import com.example.studyport.repository.MemberRepository;
 import com.example.studyport.service.StudyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.security.Principal;
 import java.util.List;
 
@@ -27,29 +31,37 @@ public class StudyController {
     private final StudyService studyService;
     private final CategoryRepository categoryRepository;
     private final MemberRepository memberRepository;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/create")
+
     public String create(Model model) {
         List<Category> categories =
-            categoryRepository.findAll();
+                categoryRepository.findAll();
         model.addAttribute("categories", categories);
 
         return "study/create";
     }
 
     @PostMapping("/create")
-    public String create(StudyDTO studyDTO, MultipartFile mainimg, Principal principal) throws IOException {
+    public String create(StudyDTO studyDTO, MultipartFile mainimg, Principal principal, Model model) throws IOException {
 
         log.info("Study create 진입");
         log.info("Study create 진입");
+        String email =
+                principal.getName();
+        log.info(email);
         log.info(principal.getName());
 
         log.info(mainimg.getOriginalFilename());
         log.info(studyDTO);
 
+        Members members =
+            memberRepository.findByEmail(email);
+        MembersDTO membersDTO =
+            modelMapper.map(members, MembersDTO.class);
+        studyDTO.setMembersDTO(membersDTO);
         studyService.create(studyDTO, mainimg);
-
-
 
 
         return "study/create";
@@ -58,5 +70,10 @@ public class StudyController {
     @GetMapping("/list")
     public String list() {
         return "study/list";
+    }
+
+    @GetMapping("/view")
+    public String view() {
+        return "study/view";
     }
 }
